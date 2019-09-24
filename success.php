ad2e7ce429119a3e3f6431934243555d2713afcc
Augusto <augusto@izus.com.ar>
2019-09-24 13:11:21 -0300

    up

--- success.php
+++ success.php
@@ -1,7 +1,5 @@
 
 
-<h1> Exito </h1> 
-
 <p> merchant_order_id: <?php echo $_POST['merchant_order_id'] ; ?>
 <p> payment_status:   <?php echo $_POST['payment_status'] ; ?>
 <p> payment_status_detail:  <?php echo $_POST['payment_status_detail'] ; ?>
